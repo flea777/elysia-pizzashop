31749f79c49e3baa504bf944068fcf2fb2dfe54a
Pedro Bim <pedrodinizz2@outlook.com>
2025-06-23 16:37:49 -0300

    Correção de erro nas migrations, uso de variáveis de ambiente

--- src/db/schema/restaurants.ts
+++ src/db/schema/restaurants.ts
@@ -1,9 +1,24 @@
 import { uuid, text, timestamp, pgTable} from 'drizzle-orm/pg-core'
+import { users } from './users'
+import { relations } from 'drizzle-orm'
 
 export const restaurants = pgTable('restaurants', {
   id: uuid('id').defaultRandom().primaryKey(),
   name: text('name').notNull(),
   description: text('description'),
+  managerId: uuid('manager_id').references(() => users.id, {
+    onDelete: 'set null',
+  }),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow()
+})
+
+export const restaurantsRelations = relations(restaurants, ({ one }) => {
+  return {
+    manager: one(users, {
+      fields: [restaurants.managerId],
+      references: [users.id],
+      relationName: 'restaurant_manager',
+    }),
+  }
 })
