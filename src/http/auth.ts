6f767f9b1df9686808e2b6196dc5fb5e967b8c09
Pedro Bim <pedrodinizz2@outlook.com>
2025-06-25 14:34:07 -0300

    Rota de logout e uso de derive

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -1,11 +1,30 @@
 import jwt from '@elysiajs/jwt'
 import Elysia from 'elysia'
 import { env } from '../env'
-import { JwtPayloadDtoSchema } from './dtos/jwt-payload.dto'
+import { JwtPayloadDtoSchema, type JwtPayloadDto } from './dtos/jwt-payload.dto'
 
 export const auth = new Elysia().use(
   jwt({
     secret: env.JWT_SECRET_KEY,
     schema: JwtPayloadDtoSchema,
   })
-)
+).derive({ as: 'scoped' }, ({ jwt, cookie: { auth } }) => {
+  return {
+    signInUser: async (payload: JwtPayloadDto) => {
+      const token = await jwt.sign(payload)
+
+      if (auth) {
+        auth.value = token
+        auth.httpOnly = true
+        auth.maxAge = 60 * 60 * 24 * 7 // 7 days
+        auth.path = '/'
+      }
+
+    },
+
+    signOutUser: () => {
+      auth?.remove()
+    }
+  }
+
+})
--- src/http/routes/authenticate-from-link.ts
+++ src/http/routes/authenticate-from-link.ts
@@ -5,59 +5,47 @@ import dayjs from 'dayjs'
 import { authLinks } from '../../db/schema'
 import { eq } from 'drizzle-orm'
 import { auth } from '../auth'
-import chalk from 'chalk'
 
-export const authenticateFromLink = new Elysia()
-.use(auth)
-.get(
-    '/auth-links/authenticate',
-    async ({ query, jwt,redirect, cookie }) => {
-      const { code, redirect: redirectUrl } = query
+export const authenticateFromLink = new Elysia().use(auth).get(
+  '/auth-links/authenticate',
+  async ({ query, redirect, signInUser }) => {
+    const { code, redirect: redirectUrl } = query
 
-      const authLinkFromCode = await db.query.authLinks.findFirst({
-        where(fields, { eq }) {
-          return eq(fields.code, code)
-        },
-      })
+    const authLinkFromCode = await db.query.authLinks.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.code, code)
+      },
+    })
 
-      if (!authLinkFromCode) {
-        throw new Error('Auth link not found.')
-      }
-
-      const daysSinceAuthLinkWasCreated = dayjs().diff(
-        authLinkFromCode.createdAt,
-        'days'
-      )
-
-      if (daysSinceAuthLinkWasCreated > 7) {
-        throw new Error('Auth link expired, please generate a new one')
-      }
-
-      const managedRestaurant = await db.query.restaurants.findFirst({
-        where(fields, { eq }) {
-          return eq(fields.managerId, authLinkFromCode.userId)
-        },
-      })
-
-      const token = await jwt.sign({
-        sub: authLinkFromCode.userId,
-        restaurantId: managedRestaurant?.id,
-      })
-      
-      cookie.body?.set({
-        value: token,
-        httpOnly: true,
-        maxAge: 60 * 60 * 24 * 7,
-        path: '/'
-      })
-      
-      console.log(chalk.yellow(`Cookie gerado: ${cookie.body}`))
-
-      await db.delete(authLinks).where(eq(authLinks.code, code))
-
-      return redirect(redirectUrl)
-    },
-    {
-      query: AuthQueryParamDtoSchema,
+    if (!authLinkFromCode) {
+      throw new Error('Auth link not found.')
     }
-  )
+
+    const daysSinceAuthLinkWasCreated = dayjs().diff(
+      authLinkFromCode.createdAt,
+      'days'
+    )
+
+    if (daysSinceAuthLinkWasCreated > 7) {
+      throw new Error('Auth link expired, please generate a new one')
+    }
+
+    const managedRestaurant = await db.query.restaurants.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.managerId, authLinkFromCode.userId)
+      },
+    })
+
+    await signInUser({
+      sub: authLinkFromCode.userId,
+      restaurantId: managedRestaurant?.id,
+    })
+
+    await db.delete(authLinks).where(eq(authLinks.code, code))
+
+    return redirect(redirectUrl)
+  },
+  {
+    query: AuthQueryParamDtoSchema,
+  }
+)
