4232d8f4358409237fdd7295d0d6b93c7972d229
Pedro Bim <pedrodinizz2@outlook.com>
2025-06-30 15:47:19 -0300

    Rota que retorna restaurante gerenciado pelo usuario logado

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,13 +4,26 @@ import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { getProfile } from './routes/get-profile'
+import { getManagedRestaurant } from './routes/get-managed-restaurant'
+import swagger from '@elysiajs/swagger'
 
 const app = new Elysia()
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: 'PizzaShop Documentation',
+          version: '1.0.0',
+        },
+      },
+    })
+  )
   .use(healthCheck)
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
   .use(getProfile)
+  .use(getManagedRestaurant)
   .listen(3333, () => {
     console.log('Http server running!')
   })
